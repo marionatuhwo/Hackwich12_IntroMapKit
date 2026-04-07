35c7fc87fe1b51ecfb66ad6505398bba72b70de9
marionatuhwo <ano@hawaii.edu>
2026-04-07 12:19:13 -1000

    Completed in class portion of Hackwich 12

--- Hackwich12_IntroMapKit/ViewController.swift
+++ Hackwich12_IntroMapKit/ViewController.swift
@@ -14,12 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     //initial location
     let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
-    let regionRadius: CLLocationDistance = 1000
+    let regionRadius: CLLocationDistance = 10000
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         centerMapOnLocation(location: initialLocation)
+        
+        //add our first annotation to the map
+        let restaurantOne = restaurantObject(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
     }
     
     func centerMapOnLocation(location: CLLocation)
--- Hackwich12_IntroMapKit/restaurantObject.swift
+++ Hackwich12_IntroMapKit/restaurantObject.swift
@@ -6,7 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
-class restaurantObject: NSObject {
+
+class restaurantObject: NSObject, MKAnnotation {
+    
+let restaurantTitle: String?
+let restaurantType: String?
+let coordinate: CLLocationCoordinate2D
+    init(title: String, type: String, coordinate: CLLocationCoordinate2D)
+    {
+        self.restaurantTitle = title
+        self.restaurantType = type
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String?
+    {
+        return restaurantTitle
+    }
+    
+    
 
 }
